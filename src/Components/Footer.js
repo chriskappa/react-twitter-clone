6909e12f694a7631e145d77ebfc7f77c4863ade3
chriskappa <chriskagkelis@yahoo.com>
2021-07-29 16:06:29 +0100

    :fire: changed the footer color

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div class="w-100 text-center mt-5 align-items-center justify-content-center">
-            <footer class="footer mt-auto py-3 bg-info">
+            <footer class="footer mt-auto py-3 bg-light ">
                 <div class="container">
                     <span class=" font-weight-bold">Made With <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" width="50"alt="" /> And &nbsp; <img src="https://help.iubenda.com/wp-content/uploads/2020/05/firebase-1024x293.png" width="100"alt="" /> &nbsp; By Christos </span>
                 </div>
